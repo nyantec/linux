4f5e9d30e2643f87b97aea890bb11bcd291d3a6b
Finn Behrens <fin@nyantec.com>
2022-12-21 10:48:33 +0100

    rust: mcom: add probe function
    Signed-off-by: Finn Behrens <fin@nyantec.com>

--- drivers/watchdog/mcom_fpga.rs
+++ drivers/watchdog/mcom_fpga.rs
@@ -16,20 +16,23 @@ kernel::module_i2c_driver! {
 
 struct MComFPGA;
 
-//kernel::define_i2c_id_table! {(), [ (DeviceId{"fpga"}, None) ]};
-
 impl i2c::Driver for MComFPGA {
     type Data = ();
 
     kernel::define_i2c_id_table! {(), [(DeviceId(b"fpga"), None),]}
 
-    fn probe(_: &mut i2c::Client, _: Option<&Self::IdInfo>) -> Result<Self::Data> {
+    fn probe(client: &mut i2c::Client, _: Option<&Self::IdInfo>) -> Result<Self::Data> {
+        dev_info!(client, "probe\n");
+        dev_info!(client, "slaveaddr: {}\n", client.get_addr());
+        if client.get_addr() != 0x3c {
+            dev_info!(client, "probe: not found\n");
+            return Err(code::EINVAL);
+        }
+
+        // Configure port 0 as output
+        let cmd = [0x06, 0x00];
+        client.master_send(&buf);
+
         Ok(())
     }
 }
-
-/*impl kernel::Module for MComFPGA {
-    fn init(_name: &'static CStr, _module: &'static ThisModule) -> Result<Self> {
-        Ok(MComFPGA)
-    }
-}*/
