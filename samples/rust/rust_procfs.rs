d3b0b9383ea0e3450cf19ce2adfef204de683ba8
Finn Behrens <fin@nyantec.com>
2022-12-21 14:39:24 +0100

    rust: samples: implement operations on proc sample

--- samples/rust/rust_procfs.rs
+++ samples/rust/rust_procfs.rs
@@ -2,9 +2,12 @@
 
 //! Rust procfs sample.
 
+use kernel::file::{self, File};
+use kernel::io_buffer::{IoBufferReader, IoBufferWriter};
 use kernel::prelude::*;
-use kernel::proc::ProcDirEntry;
+use kernel::proc::{self, ProcDirEntry};
 use kernel::str::CString;
+use kernel::Mode;
 
 module! {
     type: RustProcFS,
@@ -16,6 +19,7 @@ module! {
 
 struct RustProcFS {
     root: ProcDirEntry,
+    file: ProcDirEntry<Test>,
 }
 
 impl kernel::Module for RustProcFS {
@@ -23,6 +27,31 @@ impl kernel::Module for RustProcFS {
         let root_name = CString::try_from_fmt(fmt!("samples/{}", name))?;
         let root = ProcDirEntry::mkdir(&root_name, None)?;
 
-        Ok(Self { root })
+        let file_name = CString::try_from_fmt(fmt!("test"))?;
+        let file = ProcDirEntry::new(
+            &file_name,
+            Mode::from_int(0),
+            Some(&root),
+            Box::try_new(())?,
+        )?;
+
+        Ok(Self { root, file })
+    }
+}
+
+pub struct Test;
+#[vtable]
+impl proc::Operations for Test {
+    fn open(shared: &(), _file: &File) -> Result<()> {
+        Ok(())
+    }
+
+    fn read(shared: (), _: &File, data: &mut impl IoBufferWriter, offset: u64) -> Result<usize> {
+        if data.is_empty() || offset != 0 {
+            return Ok(0);
+        }
+
+        data.write_slice(&[1u8; 1])?;
+        Ok(1)
     }
 }
